f8a25665b8c57ac610917940f42c70414c037b37
Chris Martin <chris.martin@renaissance.com>
2025-03-28 18:02:42 -0600

    explicit foralls to be robust against constraint reordering

--- persistent-sql-lifted/library/Database/Persist/Sql/Lifted/Savepoint.hs
+++ persistent-sql-lifted/library/Database/Persist/Sql/Lifted/Savepoint.hs
@@ -21,31 +21,32 @@ import System.Random (randomRIO)
 import Prelude ((-))
 
 --  | Create a new transaction @SAVEPOINT@, returning its name
-newSavepoint :: (HasCallStack, MonadSqlBackend m) => m Text
+newSavepoint :: forall m. (HasCallStack, MonadSqlBackend m) => m Text
 newSavepoint = do
   r <- replicateM 8 randomCharacter
   let savepoint = "savepoint_" <> T.pack r
   rawExecute ("SAVEPOINT " <> savepoint) []
   pure savepoint
 
-randomCharacter :: MonadIO m => m Char
+randomCharacter :: forall m. MonadIO m => m Char
 randomCharacter = (characterSet V.!) <$> randomRIO (0, V.length characterSet - 1)
 
 characterSet :: Vector Char
 characterSet = V.fromList $ ['a' .. 'z'] <> ['1' .. '9']
 
 --  | Release a @SAVEPOINT@
-releaseSavepoint :: (HasCallStack, MonadSqlBackend m) => Text -> m ()
+releaseSavepoint :: forall m. (HasCallStack, MonadSqlBackend m) => Text -> m ()
 releaseSavepoint name = rawExecute ("RELEASE SAVEPOINT " <> name) []
 
 --  | Rollback to a @SAVEPOINT@
 rollbackToSavepoint
-  :: (HasCallStack, MonadSqlBackend m) => Text -> m ()
+  :: forall m. (HasCallStack, MonadSqlBackend m) => Text -> m ()
 rollbackToSavepoint name = rawExecute ("ROLLBACK TO SAVEPOINT " <> name) []
 
 -- | Runs a SQL action with SAVEPOINT, rolling back when specified
 rollbackWhen
-  :: (HasCallStack, MonadSqlBackend m)
+  :: forall m a
+   . (HasCallStack, MonadSqlBackend m)
   => (a -> Bool)
   -- ^ When to ROLLBACK based on the result of the action
   -> m a
